b776d1d43bdc4271184bf25f89baad33ac630d22
Phil Hord <hordp@cisco.com>
2013-11-19 17:12:07 -0500

    dda: Convert TARGET axis vars to array.
    In preparation for more efficient and scalable code using axis-loops
    for common operations, add two new array-types for signed and unsigned
    32-bit values per axis. Make the TARGET type use this array instead of
    its current X, Y, Z, and E variables.

--- dda.h
+++ dda.h
@@ -16,16 +16,12 @@
 enum axis_e { X = 0, Y, Z, E, AXIS_COUNT };
 
 /**
-  \typedef axes_uint32_t
-  \brief n-dimensional vector used to describe uint32_t axis information.
 
   Stored value can be anything unsigned. Units should be specified when declared.
 */
 typedef uint32_t axes_uint32_t[AXIS_COUNT];
 
 /**
-  \typedef axes_int32_t
-  \brief n-dimensional vector used to describe int32_t axis information.
 
   Stored value can be anything unsigned. Units should be specified when declared.
 */
