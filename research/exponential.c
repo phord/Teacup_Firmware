bb09f3e454fde3e65f3180f1543e1307e0a1a960
Phil Hord <hordp@cisco.com>
2013-12-17 19:28:40 -0500

    WIP: Partial conversion to uint64_t with some floats remaining

--- research/exponential.c
+++ research/exponential.c
@@ -5,53 +5,70 @@
 #include <stdint.h>
 #include <math.h>
 
+// Reflect times in clock cycles
+
 // vmax = max speed in steps/sec
 // acc = acceleration in steps/sec^2
-uint32_t vmax = 100 , acc = 1000 ;
-uint32_t f = 20000000;
+uint64_t vmax = 100 , acc = 1000 ;
+uint64_t f = 20000000;
 
 // ts = time to achieve vmax
-double Ts() {
-	// Trapezoidal velocity (max constant acceleration)
+uint64_t Ts() {
+	// Rampup time for trapezoidal velocity (max constant acceleration)
 	// acc * Ts = vmax
 	// ts = vmax / acc
-	return (double)vmax / (double)acc ;
+	// Note: overflow when (vmax / acc) > 200, like "vmax=2500 acc=10"
+	//       vmax=2500 is FEEDRATE=F150000, so... insane.
+	return (double)vmax * (double)f / (double)acc ;
 }
-//double ts = 2.1 * alpha ;              // Exponential velocity
 
-double Td( uint32_t dx ) {
+// ts = time to achieve vmax
+uint64_t Ts_exp() {
+	uint64_t alpha = 1.5 * 65536 ; // future
+	//double ts = 2.1 * alpha ;              // Exponential velocity
+	return (f/10 * 21 + 32768)>>65536 * alpha ;
+}
+
+uint64_t Td( uint64_t dx ) {
+	// dx in steps
 	// td = movement time as vmax
-	return (double) dx / (double)vmax ;
+	// Overflows when dx/vmax > 400
+//	uint64_t td = (f*2 / vmax + 1)>>1;
+//	return td * dx ;
+	return dx * f / vmax ;
 }
 
-double move_time( uint32_t dx ) {
-	// return time required to move dx steps
-	// td + ts = total movement time
-	return Td(dx) + Ts() ;
+uint64_t trapezoidal_velocity( uint64_t now ) {
+	// Trapezoidal velocity (max constant acceleration)
+	// @param now time of movement in ticks
+	//   acc in steps/sec/sec
+	//   return velocity in f * steps/sec
+	//   steps/sec/sec * ticks / 1000 => ticks.steps/sec/sec / 1000
+	//  Overflows at ~200000 steps/sec
+	return (uint64_t) acc * now ;
 }
 
-double trapezoidal_velocity( double now ) {
-	// Trapezoid velocity (max constant acceleration)
-	return acc * now ;
+double t(int tick) {
+	return ((double)tick)/f;
 }
 
 /* PLANNER STRUCTURE */
-double td ;
-double ts ;
-double te ;
+uint64_t td ;
+uint64_t ts ;
+uint64_t te ;
 
-double trapezoidal_position( double now ) {
+double trapezoidal_position( uint64_t now ) {
 	double pos ;
 
 	//-- Constrain to our move time
 	if ( now > te ) now = te ;
 
-	double rampup = now;
-	if ( rampup > ts) rampup = ts ;
+	double rampup = t(now);
+	if ( rampup > t(ts)) rampup = t(ts) ;
 	double cruise = 0;
-	if ( now >= td || now > ts ) cruise = now - ts ;
+	if ( t(now) >= t(td) || t(now) > t(ts) ) cruise = t(now) - t(ts) ;
 	if ( cruise < 0 ) cruise = 0;
-	double rampdown = now - td;
+	double rampdown = t(now) - t(td);
 	if ( rampdown < 0 ) rampdown = 0 ;
 
 	// Rampup period
@@ -62,32 +79,34 @@ double trapezoidal_position( double now ) {
 
 	// Rampdown period / idle period
 	pos -= (double)acc * rampdown * rampdown / 2.0;
+
+//	printf("=== %f %f %u %f %f %f ===\n", t(now), rampup, (uint64_t) (rampup * f), cruise, rampdown, pos);
+
 	return pos;
 }
 
-double velocity_profile( double now ) {
+// Note: returns ticks/sec * steps/sec / 1000
+//       Overflows at 200000 steps/sec
+uint64_t velocity_profile( uint64_t now ) {
 	// dx = steps to move
 	// now = time in secs to find velocity
-	double v = 0 ;
+	uint64_t v = 0 ;
 	if (now > te) return 0;
 	if (now < ts) v  = trapezoidal_velocity(now) ;       // Calculate trapezoidal velocity during acceleration
-	else          v = trapezoidal_velocity(ts);
+	else          v  = trapezoidal_velocity(ts);
 	if (now > td) v -= trapezoidal_velocity(now - td);
 	return v;
 }
 
-double t(int tick) {
-	return ((double)tick)/f;
-}
-
 /* Plan a movement at a given vmax, accel-max, and distance */
-void plan(uint32_t v, uint32_t a, uint32_t dx) {
+void plan(uint64_t v, uint64_t a, uint64_t dx) {
 	// Ensure:
-	// dx / vmax > vmax /acc 
+	// dx / vmax > vmax / acc
 	// dx > vmax^2 / acc
 	// dx * acc / vmax^2 > 1
 	// dx * acc > vmax^2
 	// vmax < sqrt(dx * acc)
+	// Note: ignored for small dx or acc (dx * acc <= 2), to avoid crazy slow max speed.  Is this needed?
 	if ( dx*a > 2 && dx * a < v*v) v = sqrt(dx * a);
 
 	vmax = v;
@@ -103,33 +122,29 @@ void main(void) {
 	for (dx = 35; dx < 40 ; dx += 20 ) {
 		plan(12800, 320, dx*1000);
 		double pos = 0;
-		double vprev = 0;
+		uint64_t vprev = 0;
 		int tprev = 0;
-		printf("# dx=%u  Ts=%f  Td=%f  Te=%f\n" , dx, ts, td, te );
+		printf("# dx=%u  Ts=%u  Td=%u  Te=%u\n" , dx, ts, td, te );
 		printf("# ticks, seconds, velocity, position (calculated), position (accumulated)\n");
-		for (tick = 0 ; tick < f*te ; tick+=1000 ) {
-			double v = velocity_profile(t(tick));
-			pos += (v + vprev)/2.0 * t(tick - tprev);
-			printf("%u %f %f %f %d\n", tick, t(tick), velocity_profile(t(tick)), trapezoidal_position(t(tick)), (int)(pos+0.5));
+		for (tick = 0 ; tick < te ; tick+=1000 ) {
+			uint64_t v = velocity_profile(tick);
+			pos += (double)(v + vprev)/2.0 * t(tick - tprev) / ((double)f);
+			printf("%u %f %f %f %d\n", tick, t(tick), v/(double)(f), trapezoidal_position(tick), (int)(pos+0.5));
 			vprev = v;
 			tprev = tick;
 		}
 	}
-
-	double td = Td(dx);
-	double ts = Ts();
-	double te = move_time(dx); // Td + Ts
 }
 
 void old_main(void) {
-  uint32_t start_v = 0 ;
-  uint32_t end_v = 50000 ;
-  uint32_t ticks = 20000000 ;
+  uint64_t start_v = 0 ;
+  uint64_t end_v = 50000 ;
+  uint64_t ticks = 20000000 ;
 
-  uint32_t v;
-  uint32_t x;
-  uint32_t dv = end_v - start_v;
-  uint32_t dt = ticks ;
+  uint64_t v;
+  uint64_t x;
+  uint64_t dv = end_v - start_v;
+  uint64_t dt = ticks ;
   double m = dv;
   m /= (double)dt;
 
@@ -142,17 +157,17 @@ void old_main(void) {
   // But when we fire that step occurs, we will already be at a new v and so we will be late.
   // We can find the actual next-step time with increasing accuracy, though.
   //
-  uint32_t granularity = 1000 ;
+  uint64_t granularity = 1000 ;
   for ( x = 1 ; x < ticks ; x+= granularity ) {
     //-- velocity in steps/tick (integer, rounded off)
-    uint32_t v = m*x + start_v;
+    uint64_t v = m*x + start_v;
     if ( !v ) ++v;
     
     // -- Velocity in ticks/step
-    uint32_t t = ticks / v ;
+    uint64_t t = ticks / v ;
     //-- Position (ticks-so-far * m) / 2
-    uint32_t dx = ((uint64_t)x*m)/ 2 ;
-    uint32_t nextTick = 1024.0 * ((double)ticks / sqrt((double)x * dv));
+    uint64_t dx = ((uint64_t)x*m)/ 2 ;
+    uint64_t nextTick = 1024.0 * ((double)ticks / sqrt((double)x * dv));
     printf("%u %u %u %u %u\n", x, v, t, dx , nextTick/1024);
   }
 
