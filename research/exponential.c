b82d97b48c3c7a91f594bfd01ed1da5da5d5e5e0
Phil Hord <hordp@cisco.com>
2013-12-20 12:45:39 -0500

    WIP: Use uint64_t integer math
    We need to get down to 8-bit-efficient math without any FP math.
    We lose too much precision with floats, so we have been using
    doubles.  Let's start reigning that back in.
    
    Convert the doubles back to floats.  To prevent the resulting
    accumulated error, start using all integer math for step planning
    during our piece-wise integration.  Calculate the overflow in a
    remainder variable, and determine when to step the quotient by
    comparing it to the divisor.
    
    We will use 64-bit ints for now, and try to simplify that again
    in a future commit.

--- research/exponential.c
+++ research/exponential.c
@@ -13,7 +13,7 @@
 
 // vmax = max speed in steps/sec
 // acc = acceleration in steps/sec^2
-uint64_t vmax = 100 , acc = 1000 ;
+uint64_t vmax = 0 , acc = 0 ;
 uint64_t f = 20000000;
 
 // ts = time to achieve vmax
@@ -23,13 +23,13 @@ uint64_t Ts() {
 	// ts = vmax / acc
 	// Note: overflow when (vmax / acc) > 200, like "vmax=2500 acc=10"
 	//       vmax=2500 is FEEDRATE=F150000, so... insane.
-	return (double)vmax * (double)f / (double)acc ;
+	return (float)vmax * (float)f / (float)acc ;
 }
 
 // ts = time to achieve vmax
 uint64_t Ts_exp() {
 	uint64_t alpha = 1.5 * 65536 ; // future
-	//double ts = 2.1 * alpha ;              // Exponential velocity
+	//float ts = 2.1 * alpha ;              // Exponential velocity
 	return (f/10 * 21 + 32768)>>65536 * alpha ;
 }
 
@@ -52,8 +52,8 @@ uint64_t trapezoidal_velocity( uint64_t now ) {
 	return (uint64_t) acc * now ;
 }
 
-double t(int tick) {
-	return ((double)tick)/f;
+float t(int tick) {
+	return ((float)tick)/f;
 }
 
 /* PLANNER STRUCTURE */
@@ -61,28 +61,28 @@ uint64_t td ;
 uint64_t ts ;
 uint64_t te ;
 
-double trapezoidal_position( uint64_t now ) {
-	double pos ;
+float trapezoidal_position( uint64_t now ) {
+	float pos ;
 
 	//-- Constrain to our move time
 	if ( now > te ) now = te ;
 
-	double rampup = t(now);
+	float rampup = t(now);
 	if ( rampup > t(ts)) rampup = t(ts) ;
-	double cruise = 0;
+	float cruise = 0;
 	if ( t(now) >= t(td) || t(now) > t(ts) ) cruise = t(now) - t(ts) ;
 	if ( cruise < 0 ) cruise = 0;
-	double rampdown = t(now) - t(td);
+	float rampdown = t(now) - t(td);
 	if ( rampdown < 0 ) rampdown = 0 ;
 
 	// Rampup period
-	pos = (double)acc * rampup * rampup / 2.0;
+	pos = (float)acc * rampup * rampup / 2.0;
 
 	// Cruise period
-	pos += cruise * (double)vmax;
+	pos += cruise * (float)vmax;
 
 	// Rampdown period / idle period
-	pos -= (double)acc * rampdown * rampdown / 2.0;
+	pos -= (float)acc * rampdown * rampdown / 2.0;
 
 //	printf("=== %f %f %u %f %f %f ===\n", t(now), rampup, (uint64_t) (rampup * f), cruise, rampdown, pos);
 
@@ -158,29 +158,42 @@ void main(int argc, char ** argv) {
   do_motion( v, a, d ) ;
 }
 
+
 void do_motion( int v, int a, int d ) {
-	int tick;
-	double pos = 0;
+	uint64_t tick, dTick=1000;
+	uint64_t pos = 0;
+	const uint64_t divisor = f*f*2;    // Common denominator
+	uint64_t remainder = divisor/2;  // Forward bias to round up
 	uint64_t vprev = 0;
-	int tprev = 0;
+	uint64_t tprev = 0;
 
 	plan(v, a, d);
 
 	printf("# dx=%u  Ts=%u  Td=%u  Te=%u\n" , d, ts, td, te );
 	printf("# ticks, seconds, velocity, position (calculated), position (accumulated)\n");
-	for (tick = 0 ; tick < te ; tick+=1000 ) {
+	for (tick = 0 ; tick < te ; tick+=dTick ) {
 		uint64_t v = velocity_profile(tick);
-		pos += (double)(v + vprev)/2.0 * t(tick - tprev) / ((double)f);
-		printf("%u %f %f %f %d\n", tick, t(tick), v/(double)(f), trapezoidal_position(tick), (int)(pos+0.5));
+		// Distance moved in steps*(ticks/sec)
+		remainder += (v + vprev) * (tick - tprev) ;
+		if ( remainder > divisor ) {
+			++pos ;
+			remainder -= divisor;
+		}
+		if ( remainder > divisor ) {
+			fprintf(stderr, "Step undershoot:  t=%lu  pos=%lu  remainder=%u\n", tick, pos , remainder - divisor);
+		}
+
+		printf("%u %f %f %f %d %u\n", tick, t(tick), v/(float)(f), trapezoidal_position(tick), pos, dTick );
+
 		vprev = v;
 		tprev = tick;
 	}
 
 	printf("# ticks, seconds, velocity, position (calculated), position (accumulated)\n");
 	printf("# Commanded: dx=%u  T=%u\n" , d, te );
-	printf("#    Actual: dx=%u  T=%u\n" , (int)(pos+0.5), ts, td, tick );
-	if ( (int)(pos+0.5) == d ) exit(0);
+	printf("#    Actual: dx=%u  T=%u\n" , pos, tick );
+	if ( pos == d ) exit(0);
 
 	fprintf(stderr, "Did not reach commanded distance.  demand=%u, actual=%lu\n",
-			d, (int)(pos+0.5) ) ;
+			d, pos ) ;
 }
